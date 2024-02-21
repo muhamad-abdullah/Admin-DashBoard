19df721432b898be2152151b6e43529932e9ca53
Muhamad Mansour <muhamad.abdullah.mansour@gmail.com>
2024-02-21 08:05:28 +0200

    Provide custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import "./App.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import MainRouter from "./pages/shared/Router";
+import { ColorModeContext, useMode } from "./theme";
+
 function App() {
-  return <div>Hello world</div>;
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <div className="app">
+          <CssBaseline />
+          <MainRouter />
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import MainRouter from "./pages/Global/Router.jsx";
+import App from "./App.jsx";
+import { ColorModeContext } from "./theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MainRouter />
+      <App />
   </React.StrictMode>
 );
