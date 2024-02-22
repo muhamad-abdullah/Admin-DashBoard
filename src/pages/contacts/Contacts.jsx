9255061378936c2ad507c362913ada4b108dd771
Muhamad Mansour <muhamad.abdullah.mansour@gmail.com>
2024-02-22 08:49:46 +0200

    Add Contacts screen

---
+++ src/pages/contacts/Contacts.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { Avatar, Box, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import ScreenHeader from "../shared/ScreenHeader";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { mockDataContacts } from "../../Mock/mockData";
+
+const Contacts = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [
+    { field: "id", headerName: "ID", flex: 0.1 },
+    {
+      field: "Avatar",
+      headerName: "Image",
+      renderCell: (params) => <Avatar src={params.row.Avatar} />,
+      sortable: false,
+      filterable: false,
+    },
+
+    { field: "registrarId", headerName: "Registrar ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      flex: 1,
+    },
+    {
+      field: "city",
+      headerName: "City",
+      flex: 1,
+    },
+    {
+      field: "zipCode",
+      headerName: "Zip Code",
+      flex: 1,
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <ScreenHeader
+        screenTitle="CONTACTS"
+        screenSubtitle="List of Contacts for Future Reference"
+      />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          rows={mockDataContacts}
+          columns={columns}
+          slots={{
+            toolbar: GridToolbar,
+          }}
+          slotProps={{
+            toolbar: { counter: mockDataContacts.length },
+          }}
+        ></DataGrid>
+      </Box>
+    </Box>
+  );
+};
+
+export default Contacts;
